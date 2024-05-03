07414ba9e11e4aab1c5b636a0e167dd01c46e444
MartinP1199 <potociarmartin@gmail.com>
2024-05-03 10:02:31 +0200

    feat:  workEvent foreign key

--- app/src/main/java/sk/potociarm/workguard/data/WorkEvent.kt
+++ app/src/main/java/sk/potociarm/workguard/data/WorkEvent.kt
@@ -3,10 +3,16 @@ package sk.potociarm.workguard.data
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "work_event")
+@Entity(tableName = "work_event",
+    foreignKeys = [androidx.room.ForeignKey(
+    entity = WorkTag::class,
+    childColumns = ["tagId"],
+    parentColumns = ["id"]
+)])
 data class WorkEvent (
     @PrimaryKey(autoGenerate = true)
     val id: Int,
+    val tagId: Int,
     val startTime: String,
     val endTime: String,
     var name: String,
