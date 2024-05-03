5e9613001a679943ffb62ca64c633ffa0236f8fb
MartinP1199 <potociarmartin@gmail.com>
2024-05-03 18:06:03 +0200

    chg: home screen

---
+++ app/src/main/java/sk/potociarm/workguard/ui/home/HomeScreen.kt
@@ -0,0 +1,72 @@
+package sk.potociarm.workguard.ui.home
+
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.unit.dp
+import sk.potociarm.workguard.R
+import sk.potociarm.workguard.ui.navigation.NavDestination
+
+object HomeScreen : NavDestination {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreen(
+    navigateToItemEntry: () -> Unit,
+    navigateToItemUpdate: (Int) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            //todo TopAppBar()
+        },
+        bottomBar = {
+            //todo BottomAppBar()
+        },
+        floatingActionButton = {
+            //todo FloatingActionButton()
+        },
+    ) {
+    innerPadding -> HomeBody(
+        contentPadding = innerPadding,
+        modifier = modifier.fillMaxSize(),
+        )
+    }
+}
+
+@Composable
+private fun HomeBody(
+    modifier: Modifier = Modifier,
+    //todo params
+    contentPadding: PaddingValues = PaddingValues(0.dp),
+    ) {
+    Column(
+        modifier = modifier
+        ) {
+        PrintStats()
+        PrintRunning()
+    }
+}
+
+@Composable
+fun PrintRunning() {
+    TODO("Not yet implemented")
+}
+
+@Composable
+fun PrintStats() {
+    TODO("Not yet implemented")
+}
---
+++ app/src/main/java/sk/potociarm/workguard/ui/theme/Theme.kt
@@ -0,0 +1,14 @@
+package sk.potociarm.workguard.ui.theme
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+
+@Composable
+fun WorkGuardTheme(
+    content: @Composable () -> Unit
+) {
+    MaterialTheme(
+        content = content
+    )
+    //todo theme
+}
