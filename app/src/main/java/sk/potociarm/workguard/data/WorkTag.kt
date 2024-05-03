47d0b51283c228f2333be5c4603996abb53a198d
MartinP1199 <potociarmartin@gmail.com>
2024-05-03 10:00:31 +0200

    feat:  worktag foreign key

--- app/src/main/java/sk/potociarm/workguard/data/WorkTag.kt
+++ app/src/main/java/sk/potociarm/workguard/data/WorkTag.kt
@@ -1,12 +1,19 @@
 package sk.potociarm.workguard.data
 
 import androidx.room.Entity
+import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "work_tag")
+@Entity(tableName = "work_tag",
+    foreignKeys = [ForeignKey(
+        entity = WorkTag::class,
+        childColumns = ["parentId"],
+        parentColumns = ["id"]
+    )])
 data class WorkTag (
     @PrimaryKey(autoGenerate = true)
     val id: Int,
+    val parentId: Int?,
     var name: String,
     val price: Double = 0.0
 )
