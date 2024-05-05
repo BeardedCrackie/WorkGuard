25c6b606bd779cd146cb8d9609172cdb75ce9c93
MartinP1199 <potociarmartin@gmail.com>
2024-05-05 12:49:37 +0200

    chg: dependency update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    id("com.google.devtools.ksp") version "1.9.20-1.0.14"
+    id("com.google.devtools.ksp") version "1.9.23-1.0.20"
 }
 
 android {
@@ -47,13 +47,17 @@ dependencies {
     // Import the Compose BOM
     implementation(platform("androidx.compose:compose-bom:2024.05.00"))
     implementation("androidx.activity:activity-compose:1.9.0")
-    implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
     implementation("androidx.navigation:navigation-compose:2.7.7")
+
+    // Material3
+    implementation("androidx.compose.material3:material3:1.3.0-alpha06")
+    implementation("androidx.compose.material3:material3-window-size-class:1.2.1")
+    implementation("androidx.compose.material3.adaptive:adaptive-navigation:1.0.0-alpha12")
 
     //Room
     implementation("androidx.room:room-runtime:2.6.1")
