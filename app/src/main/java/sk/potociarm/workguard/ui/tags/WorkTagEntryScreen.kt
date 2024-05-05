092017abc7763677a3d8b1a3bd6fd02f228c56a8
MartinP1199 <potociarmartin@gmail.com>
2024-05-05 20:42:28 +0200

    impl: tag entry save

--- app/src/main/java/sk/potociarm/workguard/ui/tags/WorkTagEntryScreen.kt
+++ app/src/main/java/sk/potociarm/workguard/ui/tags/WorkTagEntryScreen.kt
@@ -8,32 +8,30 @@ import androidx.compose.foundation.layout.calculateStartPadding
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
 import kotlinx.coroutines.launch
 import sk.potociarm.workguard.R
 import sk.potociarm.workguard.WorkGuardTopAppBar
+import sk.potociarm.workguard.data.worktag.WorkTag
 import sk.potociarm.workguard.ui.AppViewModelProvider
 import sk.potociarm.workguard.ui.navigation.NavDestination
+import sk.potociarm.workguard.ui.tags.component.WorkTagEditCard
 import sk.potociarm.workguard.ui.theme.WorkGuardTheme
-import java.util.Currency
-import java.util.Locale
 
 object WorkTagEntryDestination : NavDestination {
     override val route = "workTag_entry"
@@ -48,7 +46,9 @@ fun WorkTagEntryScreen(
     canNavigateBack: Boolean = true,
     viewModel: WorkTagEntryViewModel = viewModel(factory = AppViewModelProvider.Factory)
 ) {
+    val tagListState by viewModel.allTagsUiState.collectAsState()
     val coroutineScope = rememberCoroutineScope()
+
     Scaffold(
         topBar = {
             WorkGuardTopAppBar(
@@ -59,9 +59,9 @@ fun WorkTagEntryScreen(
         }
     ) { innerPadding ->
         WorkTagEntryBody(
-            workTagUiState = viewModel.workTagUiState,
-            onWorkTagValueChange = viewModel::updateUiState,
-            onSaveClick = {
+            workTagUiState = viewModel.tagUiState,
+            allTag = tagListState.tagList,
+            onSave = {
                 coroutineScope.launch {
                     viewModel.saveWorkTag()
                     navigateBack()
@@ -74,7 +74,6 @@ fun WorkTagEntryScreen(
                     top = innerPadding.calculateTopPadding()
                 )
                 .verticalScroll(rememberScrollState())
-                .fillMaxWidth()
         )
     }
 }
@@ -82,93 +81,37 @@ fun WorkTagEntryScreen(
 @Composable
 fun WorkTagEntryBody(
     workTagUiState: WorkTagUi,
-    onWorkTagValueChange: (WorkTagUi) -> Unit,
-    onSaveClick: () -> Unit,
+    allTag: List<WorkTag>,
+    onSave: () -> Unit,
     modifier: Modifier = Modifier
 ) {
     Column(
-        verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_large)),
-        modifier = modifier.padding(dimensionResource(id = R.dimen.padding_medium))
-        ) {
-        WorkTagInputForm(
+        modifier = modifier.padding(dimensionResource(id = R.dimen.padding_medium)),
+        verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_medium))
+    ) {
+        WorkTagEditCard(
             tag = workTagUiState,
-            onValueChange = onWorkTagValueChange,
-            modifier = Modifier.fillMaxWidth()
+            allTag = allTag,
         )
-        Button(
-            onClick = onSaveClick,
-            enabled = true, //todo validation
+        OutlinedButton(
+            onClick = { onSave() },
             shape = MaterialTheme.shapes.small,
             modifier = Modifier.fillMaxWidth()
         ) {
-            Text(stringResource(R.string.save_action))
+            Text(stringResource(R.string.add_work_tag))
         }
     }
 }
 
-@Composable
-fun WorkTagInputForm(
-    tag: WorkTagUi,
-    modifier: Modifier = Modifier,
-    onValueChange: (WorkTagUi) -> Unit = {},
-    enabled: Boolean = true
-) {
-    Column(
-        modifier = modifier,
-        verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_medium))
-    ) {
-        OutlinedTextField(
-            value = tag.name,
-            onValueChange = { onValueChange(tag.copy(name = it)) },
-            label = { Text(stringResource(R.string.workTag_name_req)) },
-            colors = OutlinedTextFieldDefaults.colors(
-                focusedContainerColor = MaterialTheme.colorScheme.secondaryContainer,
-                unfocusedContainerColor = MaterialTheme.colorScheme.secondaryContainer,
-                disabledContainerColor = MaterialTheme.colorScheme.secondaryContainer,
-            ),
-            modifier = Modifier.fillMaxWidth(),
-            enabled = enabled,
-            singleLine = true
-        )
-        OutlinedTextField(
-            value = tag.price.toString(),
-            onValueChange = { onValueChange(tag.copy(price = it.toDouble())) },
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Decimal),
-            label = { Text(stringResource(R.string.workTag_price)) },
-            colors = OutlinedTextFieldDefaults.colors(
-                focusedContainerColor = MaterialTheme.colorScheme.secondaryContainer,
-                unfocusedContainerColor = MaterialTheme.colorScheme.secondaryContainer,
-                disabledContainerColor = MaterialTheme.colorScheme.secondaryContainer,
-            ),
-            leadingIcon = { Text(Currency.getInstance(Locale.getDefault()).symbol) },
-            modifier = Modifier.fillMaxWidth(),
-            enabled = enabled,
-            singleLine = true
-        )
-        OutlinedTextField(
-            value = tag.parentId.toString(),
-            onValueChange = { onValueChange(tag.copy(parentId = it.toInt())) },
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Decimal),
-            label = { Text(stringResource(R.string.workTag_parent)) },
-            colors = OutlinedTextFieldDefaults.colors(
-                focusedContainerColor = MaterialTheme.colorScheme.secondaryContainer,
-                unfocusedContainerColor = MaterialTheme.colorScheme.secondaryContainer,
-                disabledContainerColor = MaterialTheme.colorScheme.secondaryContainer,
-            ),
-            modifier = Modifier.fillMaxWidth(),
-            enabled = enabled,
-            singleLine = true
-        )
-    }
-}
 
 @Preview(showBackground = true)
 @Composable
-private fun WorkTagEntryScreenPreview() {
+fun WorkEntryScreenPreview() {
     WorkGuardTheme {
         WorkTagEntryBody(
-            sampleTagUiWithParent(),
-            onWorkTagValueChange = {},
-            onSaveClick = {})
+            sampleTagUiWithoutParent(),
+            onSave = {},
+            allTag = sampleTagList(),
+        )
     }
 }
--- app/src/main/java/sk/potociarm/workguard/ui/tags/WorkTagEntryViewModel.kt
+++ app/src/main/java/sk/potociarm/workguard/ui/tags/WorkTagEntryViewModel.kt
@@ -4,20 +4,36 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 import sk.potociarm.workguard.data.worktag.WorkTagsRepository
 
 class WorkTagEntryViewModel(private val workTagsRepository: WorkTagsRepository) : ViewModel() {
+
+    val allTagsUiState: StateFlow<WorkTagUiList> =
+        workTagsRepository.getAllWorkTagsStream().map {
+            WorkTagUiList(it)
+        }.stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(TIMEOUT_MILLIS),
+            initialValue = WorkTagUiList()
+        )
+
     /**
      * Holds current workTag ui state
      */
-    var workTagUiState by mutableStateOf(WorkTagUi())
+    var tagUiState by mutableStateOf(WorkTagUi())
         private set
 
-    fun updateUiState(workTagDetails: WorkTagUi) {
-        workTagUiState = workTagDetails
+    suspend fun saveWorkTag() {
+        workTagsRepository.insertWorkTag(tagUiState.toWorkTag())
     }
 
-    suspend fun saveWorkTag() {
-        workTagsRepository.insertWorkTag(workTagUiState.toWorkTag())
+    companion object {
+        private const val TIMEOUT_MILLIS = 5_000L
     }
+
 }
