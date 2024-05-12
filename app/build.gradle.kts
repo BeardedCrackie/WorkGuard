2540ef7585e973a724ba851d16f0831b713a9f66
MartinP1199 <potociarmartin@gmail.com>
2024-05-12 14:28:46 +0200

    chg: kotlin date utils import

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -72,4 +72,6 @@ dependencies {
     //testing
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.4.0")
 }
